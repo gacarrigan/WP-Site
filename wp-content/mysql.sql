c85c23f2f992c8a8117b80c69d4acd646ca51627
pod-1198 <howdy@wpengine.com>
2012-11-03 07:45:32 +0000

    daily checkpoint

--- wp-content/mysql.sql
+++ wp-content/mysql.sql
@@ -561,8 +561,6 @@ INSERT INTO `wp_options` VALUES (11315,'_site_transient_timeout_wporg_theme_feat
 INSERT INTO `wp_options` VALUES (11316,'_site_transient_wporg_theme_feature_list','a:5:{s:6:\"Colors\";a:15:{i:0;s:5:\"black\";i:1;s:4:\"blue\";i:2;s:5:\"brown\";i:3;s:4:\"gray\";i:4;s:5:\"green\";i:5;s:6:\"orange\";i:6;s:4:\"pink\";i:7;s:6:\"purple\";i:8;s:3:\"red\";i:9;s:6:\"silver\";i:10;s:3:\"tan\";i:11;s:5:\"white\";i:12;s:6:\"yellow\";i:13;s:4:\"dark\";i:14;s:5:\"light\";}s:7:\"Columns\";a:6:{i:0;s:10:\"one-column\";i:1;s:11:\"two-columns\";i:2;s:13:\"three-columns\";i:3;s:12:\"four-columns\";i:4;s:12:\"left-sidebar\";i:5;s:13:\"right-sidebar\";}s:5:\"Width\";a:2:{i:0;s:11:\"fixed-width\";i:1;s:14:\"flexible-width\";}s:8:\"Features\";a:19:{i:0;s:8:\"blavatar\";i:1;s:10:\"buddypress\";i:2;s:17:\"custom-background\";i:3;s:13:\"custom-colors\";i:4;s:13:\"custom-header\";i:5;s:11:\"custom-menu\";i:6;s:12:\"editor-style\";i:7;s:21:\"featured-image-header\";i:8;s:15:\"featured-images\";i:9;s:15:\"flexible-header\";i:10;s:20:\"front-page-post-form\";i:11;s:19:\"full-width-template\";i:12;s:12:\"microformats\";i:13;s:12:\"post-formats\";i:14;s:20:\"rtl-language-support\";i:15;s:11:\"sticky-post\";i:16;s:13:\"theme-options\";i:17;s:17:\"threaded-comments\";i:18;s:17:\"translation-ready\";}s:7:\"Subject\";a:3:{i:0;s:7:\"holiday\";i:1;s:13:\"photoblogging\";i:2;s:8:\"seasonal\";}}','yes');
 INSERT INTO `wp_options` VALUES (11317,'_site_transient_timeout_wpas_menu','1351880884','yes');
 INSERT INTO `wp_options` VALUES (11318,'_site_transient_wpas_menu','a:5:{s:4:\"slug\";s:12:\"wp-app-store\";s:5:\"title\";s:12:\"WP App Store\";s:8:\"subtitle\";s:4:\"Home\";s:8:\"position\";s:3:\"999\";s:7:\"submenu\";a:2:{s:19:\"wp-app-store-themes\";s:6:\"Themes\";s:20:\"wp-app-store-plugins\";s:7:\"Plugins\";}}','yes');
-INSERT INTO `wp_options` VALUES (11323,'_transient_timeout_wooframework_version_data','1351881080','no');
-INSERT INTO `wp_options` VALUES (11324,'_transient_wooframework_version_data','a:2:{s:7:\"version\";s:5:\"5.4.2\";s:11:\"is_critical\";b:0;}','no');
 INSERT INTO `wp_options` VALUES (11325,'_transient_timeout_woo_framework_critical_update','1353004280','no');
 INSERT INTO `wp_options` VALUES (11326,'_transient_woo_framework_critical_update','1','no');
 INSERT INTO `wp_options` VALUES (11327,'_transient_timeout_woo_framework_critical_update_data','1353004280','no');
@@ -1448,4 +1446,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-11-02  7:48:07
+-- Dump completed on 2012-11-03  7:45:31
